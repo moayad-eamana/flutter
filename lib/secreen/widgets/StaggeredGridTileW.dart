860657aa102e7ca87ab478bba06ce6ca99117c2f
moayad-eamana <moayad.eamana@gmail.com>
2022-02-09 11:13:04 +0300

    moayad

--- lib/secreen/widgets/StaggeredGridTileW.dart
+++ lib/secreen/widgets/StaggeredGridTileW.dart
@@ -3,5 +3,5 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 Widget StaggeredGridTileW(int crossA, double hi, Widget widget) {
   return StaggeredGridTile.extent(
-      crossAxisCellCount: 1, mainAxisExtent: hi, child: widget);
+      crossAxisCellCount: crossA, mainAxisExtent: hi, child: widget);
 }
