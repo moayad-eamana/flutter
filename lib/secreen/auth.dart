4879118590296b736091b6d4ee92925132171bed
Hm00dX <said_mohd_2005@hotmail.com>
2022-02-23 09:28:52 +0300

    AuthenticateBio

--- lib/secreen/auth.dart
+++ lib/secreen/auth.dart
@@ -1,5 +1,7 @@
+import 'package:eamanaapp/utilities/globalcss.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:local_auth/auth_strings.dart';
 
 import 'package:local_auth/local_auth.dart';
 
@@ -17,6 +19,10 @@ class _AuthenticateBioState extends State<AuthenticateBio> {
   // List<BiometricType>? _availableBiometrics;
   String _authorized = 'Not Authorized';
   bool _isAuthenticating = false;
+
+  AndroidAuthMessages androidAuthStrings =
+      const AndroidAuthMessages(signInTitle: "الرجاء التحقق من هويتك");
+  IOSAuthMessages iOSAuthStrings = const IOSAuthMessages();
 
   @override
   void initState() {
@@ -36,12 +42,18 @@ class _AuthenticateBioState extends State<AuthenticateBio> {
         _authorized = 'Authenticating';
       });
       authenticated = await auth.authenticate(
-          localizedReason: 'Let OS determine authentication method',
+          androidAuthStrings: androidAuthStrings,
+          localizedReason: 'تحقق',
           useErrorDialogs: true,
           stickyAuth: true);
       setState(() {
         _isAuthenticating = false;
       });
+      if (_isAuthenticating == false) {
+        print("suc 1");
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
+      }
     } on PlatformException catch (e) {
       print(e);
       setState(() {
@@ -50,9 +62,7 @@ class _AuthenticateBioState extends State<AuthenticateBio> {
       });
       return;
     }
-    if (!mounted) {
-      return;
-    }
+    if (!mounted) {}
 
     setState(
         () => _authorized = authenticated ? 'Authorized' : 'Not Authorized');
@@ -60,14 +70,34 @@ class _AuthenticateBioState extends State<AuthenticateBio> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SafeArea(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("TTTTTTTTT"),
-          ElevatedButton(
-            onPressed: _authenticate,
-            child: Text("ath"),
-          )
+          Text(
+            "تسجيل الدخول بواسطة البصمة",
+            style: titleTx(baseColor),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          IconButton(
+              iconSize: 100,
+              onPressed: _authenticate,
+              icon: Icon(
+                Icons.fingerprint,
+              )),
+          SizedBox(
+            height: 100,
+          ),
+          Image(
+            //width: responsiveMT(90, 150),
+            alignment: Alignment.center,
+            width: 150,
+
+            image: AssetImage("assets/image/rakamy-logo-2.png"),
+          ),
         ],
       ),
     );
