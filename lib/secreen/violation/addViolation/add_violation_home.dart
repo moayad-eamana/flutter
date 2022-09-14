8383e3839fdc2b4b6687da936d2e8df1423adcec
moayad-eamana <moayad.eamana@gmail.com>
2022-09-14 09:02:13 +0300

    violation structure

---
+++ lib/secreen/violation/addViolation/add_violation_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class add_violation extends StatefulWidget {
+  @override
+  State<add_violation> createState() => _add_violationState();
+}
+
+class _add_violationState extends State<add_violation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
