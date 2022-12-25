71046f27b6659965d5f6d9453e65fea1f610f916
Hm00dX <said_mohd_2005@hotmail.com>
2022-12-25 11:23:55 +0300

    push

--- lib/secreen/RequestsHrHistory.dart/desclaimer_datiels.dart
+++ lib/secreen/RequestsHrHistory.dart/desclaimer_datiels.dart
@@ -33,8 +33,7 @@ class _DesclaimerdatielsState extends State<Desclaimerdatiels> {
     );
     var respons;
     if (sharedPref.getString("dumyuser") != "10284928492") {
-      respons =
-          await getAction("HR/GetEvacuationDetails/" + widget.RequestNumber);
+      respons = await getAction("HR/GetEvacuationDetails/" + "28");
       GetEvacuationDetails = jsonDecode(respons.body)["ApprovalsList"];
     } else {
       GetEvacuationDetails = [];
@@ -63,75 +62,116 @@ class _DesclaimerdatielsState extends State<Desclaimerdatiels> {
                           itemCount: GetEvacuationDetails.length,
                           itemBuilder: (context, index) {
                             return Container(
-                              margin: EdgeInsets.all(10),
+                              margin: EdgeInsets.all(8),
                               child: Card(
                                 color: BackGColor,
                                 elevation: 5,
-                                child: ConstrainedBox(
-                                  constraints: new BoxConstraints(
-                                    minHeight: 210.0,
+                                child: ListTile(
+                                  // leading: Text(GetEvacuationDetails[index]
+                                  //         ["TransactionDate"]
+                                  //     .toString()
+                                  //     .split("T")[0]),
+                                  leading: Text(
+                                    GetEvacuationDetails[index]["Status"]
+                                                    .toString() ==
+                                                "جارى الإعتماد" ||
+                                            GetEvacuationDetails[index]
+                                                        ["Status"]
+                                                    .toString() ==
+                                                ""
+                                        ? "لم يتم تحديد"
+                                        : GetEvacuationDetails[index]
+                                                ["TransactionDate"]
+                                            .toString()
+                                            .split("T")[0],
+                                    style: descTx1(baseColorText),
                                   ),
-                                  child: new DecoratedBox(
-                                    decoration: new BoxDecoration(
-                                      color: BackGColor,
-                                    ),
-                                    child: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Table(
-                                              border: TableBorder.all(),
-                                              columnWidths: {
-                                                0: FlexColumnWidth(1.5),
-                                                1: FlexColumnWidth(4),
-                                              },
-                                              children: <TableRow>[
-                                                TableRowW(
-                                                    "رقم الحركة",
-                                                    GetEvacuationDetails[index]
-                                                            ["TransactionID"]
-                                                        .toString()),
-                                                TableRowW(
-                                                    "الإدارة",
-                                                    GetEvacuationDetails[index]
-                                                            ["DepartmentName"]
-                                                        .toString()),
-                                                TableRowW(
-                                                    "تاريخ الإجراء",
-                                                    GetEvacuationDetails[index][
-                                                                        "Status"]
-                                                                    .toString() ==
-                                                                "جارى الإعتماد" ||
-                                                            GetEvacuationDetails[
-                                                                            index]
-                                                                        [
-                                                                        "Status"]
-                                                                    .toString() ==
-                                                                ""
-                                                        ? "لم يتم تحديد"
-                                                        : GetEvacuationDetails[
-                                                                    index][
-                                                                "TransactionDate"]
-                                                            .toString()
-                                                            .split("T")[0]),
-                                                TableRowW(
-                                                    "الحالة",
-                                                    GetEvacuationDetails[index]
-                                                            ["Status"]
-                                                        .toString()),
-                                                TableRowW(
-                                                    "الوصف",
-                                                    GetEvacuationDetails[index]
-                                                            ["Description"]
-                                                        .toString()),
-                                              ]),
-                                        ],
-                                      ),
-                                    ),
+                                  title: Text(
+                                    GetEvacuationDetails[index]
+                                            ["DepartmentName"]
+                                        .toString(),
+                                    style: descTx1(baseColor),
                                   ),
+                                  subtitle: Text(
+                                    GetEvacuationDetails[index]["Status"]
+                                            .toString() +
+                                        " - " +
+                                        GetEvacuationDetails[index]
+                                                ["Description"]
+                                            .toString(),
+                                    style: descTx2(secondryColorText),
+                                  ),
+                                  // isThreeLine: true,
+                                  // leading: Text(
+                                  //   naif[index]["RequestNumber"].toString(),
+                                  //   style: titleTx(baseColor),
+                                  // ),
                                 ),
+                                // ConstrainedBox(
+                                //   constraints: new BoxConstraints(
+                                //     minHeight: 210.0,
+                                //   ),
+                                //   child: new DecoratedBox(
+                                //     decoration: new BoxDecoration(
+                                //       color: BackGColor,
+                                //     ),
+                                //     child: Padding(
+                                //       padding: EdgeInsets.all(10),
+                                //       child: Column(
+                                //         crossAxisAlignment:
+                                //             CrossAxisAlignment.center,
+                                //         children: [
+                                //           Table(
+                                //               border: TableBorder.all(),
+                                //               columnWidths: {
+                                //                 0: FlexColumnWidth(1.5),
+                                //                 1: FlexColumnWidth(4),
+                                //               },
+                                //               children: <TableRow>[
+                                //                 TableRowW(
+                                //                     "رقم الحركة",
+                                //                     GetEvacuationDetails[index]
+                                //                             ["TransactionID"]
+                                //                         .toString()),
+                                //                 TableRowW(
+                                //                     "الإدارة",
+                                //                     GetEvacuationDetails[index]
+                                //                             ["DepartmentName"]
+                                //                         .toString()),
+                                //                 TableRowW(
+                                //                     "تاريخ الإجراء",
+                                //                     GetEvacuationDetails[index][
+                                //                                         "Status"]
+                                //                                     .toString() ==
+                                //                                 "جارى الإعتماد" ||
+                                //                             GetEvacuationDetails[
+                                //                                             index]
+                                //                                         [
+                                //                                         "Status"]
+                                //                                     .toString() ==
+                                //                                 ""
+                                //                         ? "لم يتم تحديد"
+                                //                         : GetEvacuationDetails[
+                                //                                     index][
+                                //                                 "TransactionDate"]
+                                //                             .toString()
+                                //                             .split("T")[0]),
+                                //                 TableRowW(
+                                //                     "الحالة",
+                                //                     GetEvacuationDetails[index]
+                                //                             ["Status"]
+                                //                         .toString()),
+                                //                 TableRowW(
+                                //                     "الوصف",
+                                //                     GetEvacuationDetails[index]
+                                //                             ["Description"]
+                                //                         .toString()),
+                                //               ]),
+                                //         ],
+                                //       ),
+                                //     ),
+                                //   ),
+                                // ),
                               ),
                             );
                           })
