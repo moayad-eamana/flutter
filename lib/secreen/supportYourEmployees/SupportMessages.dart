7536bbef64ea1808b93a8fa84f8c8b17e18d2a18
Hm00dX <said_mohd_2005@hotmail.com>
2023-02-27 08:46:07 +0300

    add onTap action

--- lib/secreen/supportYourEmployees/SupportMessages.dart
+++ lib/secreen/supportYourEmployees/SupportMessages.dart
@@ -1,6 +1,7 @@
 import 'package:eamanaapp/utilities/globalcss.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:eamanaapp/secreen/widgets/widgetsUni.dart';
 
 class SupportMessages extends StatefulWidget {
   List list;
@@ -25,14 +26,28 @@ class _SupportMessagesState extends State<SupportMessages> {
     return ListView.builder(
         itemCount: widget.list.length,
         itemBuilder: (context, index) {
-          return Container(
-            decoration: containerdecoration(Colors.white),
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            width: 100.w,
-            child: Text(
-              widget.list[index],
-              style: subtitleTx(baseColor),
+          return GestureDetector(
+            onTap: () {
+              print(widget.list[index]);
+            },
+            child: Container(
+              decoration: containerdecoration(Colors.white),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(6),
+              width: 60.w,
+              child: Row(
+                children: [
+                  Text(
+                    widget.list[index],
+                    style: subtitleTx(baseColor),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.send,
+                    color: baseColor,
+                  )
+                ],
+              ),
             ),
           );
         });
