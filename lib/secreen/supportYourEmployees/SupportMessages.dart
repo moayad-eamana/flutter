855a7370e1899b8e02db944d004e9c4dcedd7c20
Hm00dX <said_mohd_2005@hotmail.com>
2023-03-08 09:19:56 +0300

    Flexible text to view more lines message

--- lib/secreen/supportYourEmployees/SupportMessages.dart
+++ lib/secreen/supportYourEmployees/SupportMessages.dart
@@ -23,33 +23,41 @@ class _SupportMessagesState extends State<SupportMessages> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: widget.list.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              print(widget.list[index]);
-            },
-            child: Container(
-              decoration: containerdecoration(Colors.white),
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(6),
-              width: 60.w,
-              child: Row(
-                children: [
-                  Text(
-                    widget.list[index],
-                    style: subtitleTx(baseColor),
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.send,
-                    color: baseColor,
-                  )
-                ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+          itemCount: widget.list.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                print(widget.list[index]);
+              },
+              child: Container(
+                decoration: containerdecoration(BackGWhiteColor),
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                width: 60.w,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        widget.list[index],
+                        style: subtitleTx(baseColor),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.send,
+                        color: baseColor,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        });
+            );
+          }),
+    );
   }
 }
