df7e469da213601ffc481e4b8e1c8dbe5d274840
Hm00dX <said_mohd_2005@hotmail.com>
2023-03-29 11:35:56 +0300

    qrcodelist clean

--- lib/utilities/QRCodeList.dart
+++ lib/utilities/QRCodeList.dart
@@ -15,24 +15,6 @@ class QrcodeList {
         "Navigation": "",
         "icon": "assets/SVGs/qr_code_scanner.svg",
         "Action": () {
-          // Navigator.pushNamed(context, "/scannQrcode");
-          // dynamic MyData = {
-          //   "id": 1,
-          //   "Name": sharedPref.getString("FirstName").toString() +
-          //       " " +
-          //       sharedPref.getString("LastName").toString(),
-          //   "Title": sharedPref.getString("Title").toString() == ""
-          //       ? sharedPref.getString("JobName").toString()
-          //       : sharedPref.getString("Title").toString(),
-          //   "imageUrl": "https://archive.eamana.gov.sa/TransactFileUpload" +
-          //       sharedPref.getString("ImageURL").toString(),
-          //   "PhoneNum": sharedPref.getString("MobileNumber") ?? "",
-          //   "Email": sharedPref.getString("Email").toString(),
-          //   "Extension": sharedPref.getInt("Extension").toString() ?? "",
-          //   "ID": EmployeeProfile.getEmployeeNumber(),
-          //   "GenderID": sharedPref.getInt("GenderID"),
-          // };
-
           BottomSheetQrCode("بيانتي", getempdata(), context);
         }
       },
@@ -41,22 +23,6 @@ class QrcodeList {
         "Navigation": "",
         "icon": "assets/SVGs/qr_code_scanner.svg",
         "Action": () {
-          // Navigator.pushNamed(context, "/scannQrcode");
-          // var vCard = VCard();
-
-          // vCard.firstName = sharedPref.getString("FirstName").toString();
-          // vCard.lastName = sharedPref.getString("LastName").toString();
-          // vCard.cellPhone = sharedPref.getString("MobileNumber").toString();
-          // vCard.email =
-          //     sharedPref.getString("Email").toString() + "@eamana.gov.sa";
-          // vCard.organization = "أمانة المنطقة الشرقية";
-          // vCard.jobTitle = sharedPref.getString("Title") == "" ||
-          //         sharedPref.getString("Title") == null
-          //     ? sharedPref.getString("JobName")
-          //     : sharedPref.getString("Title");
-
-          // String vcarddate = vCard.getFormattedString();
-
           BottomSheetQrCode("جهة الاتصال", getvcarddate(), context);
         }
       },
