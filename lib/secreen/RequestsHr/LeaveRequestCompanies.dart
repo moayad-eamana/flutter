e67fb36ceadd501089e92fb8c7b7fdbcc2ef1a7d
noorAlsaud <noor1418h@gmail.com>
2023-07-12 15:15:37 +0300

    LeaveRequest page

--- lib/secreen/RequestsHr/LeaveRequestCompanies.dart
+++ lib/secreen/RequestsHr/LeaveRequestCompanies.dart
@@ -1,4 +1,13 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:eamanaapp/secreen/widgets/widgetsUni.dart';
+import 'package:eamanaapp/model/employeeInfo/EmployeeProfle.dart';
+import 'package:eamanaapp/secreen/widgets/appbarW.dart';
+import 'package:eamanaapp/utilities/constantApi.dart';
+import 'package:eamanaapp/utilities/globalcss.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class LeaveRequestCompanies extends StatefulWidget {
   const LeaveRequestCompanies({Key? key}) : super(key: key);
@@ -8,8 +17,169 @@ class LeaveRequestCompanies extends StatefulWidget {
 }
 
 class _LeaveRequestCompaniesState extends State<LeaveRequestCompanies> {
+  TextEditingController _date = TextEditingController();
+  TextEditingController _note = TextEditingController();
+  List LeaveRequestTypes = ["حضوري", "انصراف"];
+  var resBody;
+  Future<void> getData() async {
+    String empNo = await EmployeeProfile.getEmployeeNumber();
+
+    var respose = await getAction(
+        "HR/GetCompanyEmployeeReplacementList/" + empNo); // to test response
+    print(respose);
+
+    //resBody = jsonDecode(respose.body)["body"];
+  }
+
+  @override
+  void initState() {
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+          // backgroundColor: Colors.transparent,
+          appBar: AppBarW.appBarW("تقدیم طلب استئذان", context, null),
+          body: Stack(
+            children: [
+              widgetsUni.bacgroundimage(),
+              SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: BackGWhiteColor,
+                      border: Border.all(
+                        color: bordercolor,
+                      ),
+                      //color: baseColor,
+                      borderRadius: BorderRadius.all(
+                        new Radius.circular(4),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Text(
+                              "نحتاج بس البیانات التالیة لاستكمال طلب الاستئذان",
+                              style: descTx1(secondryColorText),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          StaggeredGrid.count(
+                              crossAxisCount: responsiveGrid(1, 2),
+                              mainAxisSpacing: 10,
+                              crossAxisSpacing: 10,
+                              children: [
+                                TextFormField(
+                                  controller: _date,
+                                  style: TextStyle(
+                                    color: baseColorText,
+                                  ),
+                                  readOnly: true,
+                                  // keyboardType: TextInputType.datetime,
+                                  maxLines: 1,
+                                  decoration: formlabel1("تاریخ الاستئذان"),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'فضلاً أدخل التاريخ  ';
+                                    }
+                                    return null;
+                                  },
+                                  onTap: () {
+                                    DatePicker.showDatePicker(context,
+                                        theme: DatePickerTheme(
+                                          backgroundColor: BackGWhiteColor,
+                                          itemStyle: TextStyle(
+                                            color: baseColorText,
+                                          ),
+                                        ),
+                                        showTitleActions: true,
+                                        minTime: DateTime(2021, 3, 5),
+                                        onChanged: (date) {
+                                      _date.text =
+                                          date.toString().split(" ")[0];
+                                      print('change $date');
+                                    }, onConfirm: (date) {
+                                      _date.text =
+                                          date.toString().split(" ")[0];
+                                      print('confirm $date');
+                                    },
+                                        currentTime: DateTime.now(),
+                                        locale: LocaleType.ar);
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                              ]),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  "نوع الاستئذان ",
+                                  style: descTx1(baseColorText),
+                                  maxLines: 3,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              ToggleSwitch(
+                                radiusStyle: true,
+                                borderWidth: 1,
+                                borderColor: [bordercolor],
+                                inactiveBgColor: BackGColor,
+                                inactiveFgColor: baseColorText,
+                                activeBgColor: [baseColor],
+                                totalSwitches: 2,
+                                labels: ['حضوري', 'انصراف'],
+                                onToggle: (index) {
+                                  int indexS = index as int;
+                                  print('switched to: ');
+                                },
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  "الموظف البديل ",
+                                  style: descTx1(baseColorText),
+                                  maxLines: 3,
+                                ),
+                                //DROPDOWN LIST WITH SEARCH FUNCTION
+                              ),
+                            ],
+                          ),
+                          TextFormField(
+                            controller: _note,
+                            keyboardType: TextInputType.text,
+                            maxLines: 3,
+                            style: TextStyle(color: baseColorText),
+                            decoration: formlabel1("ملاحظات"),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
